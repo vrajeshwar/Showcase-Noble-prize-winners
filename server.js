b83723982319f317b0186f2cb293b440ca64eecb
AkshayC <AkshayC@github.com>
2019-11-19 19:57:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/bikayi-assignment'));
+// console.log("using the file:" + __dirname + '/dist/bikayi-assignment')
+
+
+// app.get('/*', function (req, res) {
+//   console.log("sending index.html");
+//   res.sendFile(path.join(__dirname + '/dist/bikayi-assignment/index.html'));
+// });
+
+// Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080 || 3000);
+
+// console.log("Console listening!");
+
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,16 +27,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/bikayi-assignment'));
-console.log("using the file:" + __dirname + '/dist/bikayi-assignment')
-
+app.use(express.static('./dist/bikayi-assignment'));
 
 app.get('/*', function (req, res) {
-  console.log("sending index.html");
-  res.sendFile(path.join(__dirname + '/dist/bikayi-assignment/index.html'));
+
+  res.sendFile(path.join(__dirname, '/dist/bikayi-assignment/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080 || 3000);
-
-console.log("Console listening!");
+app.listen(process.env.PORT || 8080);
