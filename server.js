cfde05348131b9d415370cf0f29113f4aed5d355
AkshayC <AkshayC@github.com>
2019-11-19 18:42:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(express.static(__dirname + '/dist/bikayi-assignment'));
 
 
 
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/bikayi-assignment/index.html'));
-// });
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/bikayi-assignment/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
