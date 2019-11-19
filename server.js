a03222964cd70c7fc149723f4e9e6663acb3b052
AkshayC <AkshayC@github.com>
2019-11-19 19:46:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/bikayi-assignment'));
-
+console.log("using the file:" + __dirname + '/dist/bikayi-assignment')
 
 
 app.get('/*', function (req, res) {
+  console.log("sending index.html");
   res.sendFile(path.join(__dirname + '/dist/bikayi-assignment/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080 || 3000);
 
 console.log("Console listening!");
