540d3cc9dccc22d568722262e125316e86a5700e
AkshayC <AkshayC@github.com>
2019-11-19 15:00:00 +0530

    Adding pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,10 @@ export class AppComponent implements OnInit, OnDestroy {
   nobelprizeSub: Subscription;
   entireResponse: any;
   filteredResponse = [];
+  filteredPaginatedResponse = [];
   theGreatOnes = [];
+  buttonHeaderTitle = 'Fetch Nobel Prize Winners';
+  displayAmount = 10;
 
   constructor(private http: HttpClient) {
 
@@ -29,7 +32,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
 
   filterBy(Category: string) {
-
+    this.buttonHeaderTitle = Category;
     if (Category === 'none') {
       this.filteredResponse = [];
     } else if (Category === 'all') {
@@ -45,8 +48,46 @@ export class AppComponent implements OnInit, OnDestroy {
       }
       this.filteredResponse = tempArray;
     }
+    
+    this.displayItemList(this.displayAmount);
   }
 
+  displayItemList(amount: number) {
+    this.displayAmount = amount;
+    const tempArray = [];
+    for (let i = 0; i < amount; i++) {
+      tempArray.push(this.filteredResponse[i]);
+    }
+    
+    this.filteredPaginatedResponse = tempArray;
+  }
+
+  nextPage() {
+    const tempArray = [];
+    const index = this.filteredResponse.indexOf(this.filteredPaginatedResponse[this.displayAmount - 1]) + 1;// 10
+    let lastElement = index + this.displayAmount; //20
+    if((lastElement - this.filteredResponse.length) < this.displayAmount){
+       lastElement = lastElement - this.filteredResponse.length;
+    }
+    for (let i = index; i < lastElement; i++) {
+      tempArray.push(this.filteredResponse[i]);
+    }
+    
+    
+    this.filteredPaginatedResponse = tempArray;
+  }
+
+  previousPage() {
+    const tempArray = [];
+    const value = this.filteredResponse.indexOf(this.filteredPaginatedResponse[0]) - 1;
+    const index = value  - this.displayAmount + 1;
+    
+    for (let i = index; i <= value; i++) {
+      tempArray.push(this.filteredResponse[i]);
+    }
+           
+    this.filteredPaginatedResponse = tempArray;
+  }
 
   fetchByYear(value: string) {
     const tempArray = [];
@@ -87,7 +128,7 @@ export class AppComponent implements OnInit, OnDestroy {
       }
     }
     this.theGreatOnes.splice(5, 1);
-    console.log(this.theGreatOnes);
+   
   }
 
   ngOnDestroy() {
